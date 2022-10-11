a6ea3af6793bdd8f394f0ed4a87ce75bdf7c2058
Callum Halton <ch936@cam.ac.uk>
2022-10-11 09:41:22 +0100

    Improves pseudocode and finishes hall effect test

--- Tests/sensor_tests/sensor_tests.ino
+++ Tests/sensor_tests/sensor_tests.ino
@@ -1,6 +1,7 @@
 int sensorPin = A0;   
 int sensorValue = 0;
-int sample_length = 100;
+int avg = 0;
+const int sample_length = 200;
 int vals[sample_length];
 int i = 0;
 
@@ -11,16 +12,20 @@ void setup() {
 
 void loop() {
   sensorValue = analogRead(sensorPin);
+ 
   if (i < sample_length) {
-    vals[i] = sensorValue
-    i++
+    vals[i] = sensorValue;
+    i++;
   } else {
-    for (j=0; j < sample_length-1, j++) {
-      
+    int tot = sensorValue;
+    for (int j=1; j < sample_length; j++) {
+      tot += vals[j];
+      vals[j-1] = vals[j];
     }
-    vals[sample_length - 1] = sensorValue
+    vals[sample_length - 1] = sensorValue;
+    avg = tot / sample_length;
   }
-  vals[i] = sensorValue
-  Serial.println(sensorValue);
+
+  Serial.println(avg);
   delay(20);
 }
